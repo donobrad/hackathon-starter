e74ce081dd212947df58d9f5cb109d048dd2c555
Donovan Bradley <dbjpk12@gmail.com>
2019-11-08 23:05:35 -0700

    added full name

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '18.208.169.153',
+      host: '34.205.72.129',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
